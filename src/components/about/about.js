f6f8e6b8b02f46b28bd321e090825fc2ace451d9
kamilsen <sendej@gmail.com>
2021-05-13 09:28:58 +0200

    podstawowe ustawienia karuzeli ze zdjęciami

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import Carousel from './carousel/carousel'
+
+import styles from './about.module.scss'
+
 const about = () => (
     <>
-        <div>slider</div>
-        <div>podstawowe informacje</div>
+        <div className={styles.about}>
+            <div className={styles.carouselWrapper}>
+                <Carousel/>
+            </div>
+            <div>podstawowe informacje</div>
+        </div>
     </>
 )
 
